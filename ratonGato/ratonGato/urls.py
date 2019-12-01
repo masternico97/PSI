f3987697c7f2088f32b727defc33cf8d84f813e1
Estudiante Escuela Politecnica Superior <nicolasserranosalas@gmail.com>
2019-12-01 23:16:13 +0100

    CSS hecho mas o menos pero no tengo gusto

--- ratonGato/ratonGato/urls.py
+++ ratonGato/ratonGato/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from logic import views
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,7 +35,6 @@ urlpatterns = [
     path('move/', views.move, name='move'),
 ]
 
-"""
-    urlpatterns += static(settings.STATIC_URL,
+urlpatterns += static(settings.STATIC_URL,
                       document_root=settings.STATIC_ROOT)
-"""
+
