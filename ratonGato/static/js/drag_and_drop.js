30dcb5354d7a8831249673f82e911e0ba77a1c56
Martin Salinas <martin.salinas@estudiante.uam.es>
2019-12-06 20:50:26 +0100

    Malfunctioning drag and drop

--- ratonGato/static/js/drag_and_drop.js
+++ ratonGato/static/js/drag_and_drop.js
@@ -1,14 +1,31 @@
-// $(document).ready(function(){
-// 		$('.draggable').on("dragstart", function (draggable) {
-// 			  var dt = draggable.originalEvent.dataTransfer;
-// 			  dt.setData('Text', $(this).attr('id'));
-// 			});
-// 	    $('table td').on("dragenter dragover drop", function (draggable) {
-// 		   event.preventDefault();
-// 		   if (draggable.type === 'drop') {
-// 			  var data = draggable.originalEvent.dataTransfer.getData('Text',$(this).attr('id'));
-// 			  de=$('#'+data).detach();
-// 			  de.appendTo($(this));
-// 		   };
-// 	   });
-// })
+$(document).ready(function(){
+		$('.draggable').on("dragstart", function (draggable) {
+			  var dt = draggable.originalEvent.dataTransfer;
+			  dt.setData('Text', $(this).attr('id'));
+			});
+	    $('table td').on("dragenter dragover drop", function (draggable) {
+		   event.preventDefault();
+		   if (draggable.type === 'drop') {
+			  var data = draggable.originalEvent.dataTransfer.getData('Text',$(this).attr('id'));
+              alert(draggable.originalEvent.dataTransfer.getData('Text',$(this).attr('id')));
+			  de=$('#'+data).detach();
+			  de.appendTo($(this));
+		   };
+	   });
+})
+
+// $( init );
+//
+// function init() {
+//   $('.draggable').draggable( {
+//     cursor: 'move',
+//     containment: 'document',
+//     stop: handleDragStop
+//   } );
+// }
+//
+// function handleDragStop( event, ui ) {
+//   var offsetXPos = parseInt( ui.offset.left );
+//   var offsetYPos = parseInt( ui.offset.top );
+//   alert( "Drag stopped!nnOffset: (" + offsetXPos + ", " + offsetYPos + ")n");
+// }
