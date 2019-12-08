41c82a948ab7356015886dd9a814a32b2571f3b8
Martin Salinas <martin.salinas@estudiante.uam.es>
2019-12-08 03:55:30 +0100

    Fully functional drag & drop

--- ratonGato/static/js/drag_and_drop.js
+++ ratonGato/static/js/drag_and_drop.js
@@ -1,31 +1,28 @@
-$(document).ready(function(){
-		$('.draggable').on("dragstart", function (draggable) {
-			  var dt = draggable.originalEvent.dataTransfer;
-			  dt.setData('Text', $(this).attr('id'));
-			});
-	    $('table td').on("dragenter dragover drop", function (draggable) {
-		   event.preventDefault();
-		   if (draggable.type === 'drop') {
-			  var data = draggable.originalEvent.dataTransfer.getData('Text',$(this).attr('id'));
-              alert(draggable.originalEvent.dataTransfer.getData('Text',$(this).attr('id')));
-			  de=$('#'+data).detach();
-			  de.appendTo($(this));
-		   };
-	   });
-})
+$(document).ready(function() {
+	var initial_position = -1;
+	var final_position = -1;
 
-// $( init );
-//
-// function init() {
-//   $('.draggable').draggable( {
-//     cursor: 'move',
-//     containment: 'document',
-//     stop: handleDragStop
-//   } );
-// }
-//
-// function handleDragStop( event, ui ) {
-//   var offsetXPos = parseInt( ui.offset.left );
-//   var offsetYPos = parseInt( ui.offset.top );
-//   alert( "Drag stopped!nnOffset: (" + offsetXPos + ", " + offsetYPos + ")n");
-// }
+	$('.draggable').on("dragstart", function (draggable) {
+          var elemId = $(this).parent().attr("id");
+		  var img = $(this).attr("id");
+		  initial_position = elemId.substring(11, elemId.length);
+		  var gif = document.createElement("img");
+		  gif.src = "http://kryogenix.org/images/hackergotchi-simpler.png";
+		  draggable.dataTransfer.setDragImage(gif, 0, 0);
+		  //document.getElementById(img).style.visibility = "hidden";
+		  //document.getElementById(img).hidden = true;
+    });
+
+	$('.blank_cell').on("dragenter dragover drop", function (draggable) {
+		draggable.preventDefault();
+		if (draggable.type === 'drop') {
+			var elemId = $(this).parent().attr("id");
+			final_position = elemId.substring(5, elemId.length);
+			document.getElementById("id_origin").value = initial_position;
+			document.getElementById("id_target").value = final_position;
+			initial_position = -1;
+			final_position = -1;
+			document.getElementById("move_button").click();
+		}
+	});
+});
